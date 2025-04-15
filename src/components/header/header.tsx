d9df8feaf81b53ded689fcd4aeca58895ff82024
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-04-15 23:11:46 +0200

    updt header.tsx - header responsive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,37 +3,67 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <>
-      <nav
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          fontSize: "1.5em",
-          width: "100%",
-          padding: "1rem",
-          backgroundColor: "#D6CBAF",
-        }}
-      >
-        <div
-          style={{
-            width: "20%",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
+      <nav class="header-nav">
+        <div class="header-logo">
           <a href="/">Micky MARI</a>
         </div>
 
-        <div
-          style={{
-            width: "30%",
-            display: "flex",
-            justifyContent: "space-around",
-          }}
-        >
+        <div class="header-links">
           <a href="/projet">Projets</a>
           <a href="/about">À Propos</a>
         </div>
       </nav>
+
+      <style>
+        {`
+          .header-nav {
+            display: flex;
+            justify-content: space-between;
+            font-size: 1.5em;
+            width: 100%;
+            padding: 1rem;
+            background-color: #D6CBAF;
+            flex-wrap: wrap;
+          }
+
+          .header-logo {
+            width: 20%;
+            display: flex;
+            justify-content: center;
+            min-width: 150px;
+          }
+
+          .header-links {
+            width: 30%;
+            display: flex;
+            justify-content: space-around;
+            min-width: 300px;
+          }
+
+          @media (max-width: 768px) {
+            .header-nav {
+              flex-direction: column;
+              align-items: center;
+            }
+
+            .header-logo,
+            .header-links {
+              width: 100%;
+              justify-content: center;
+              margin-bottom: 10px;
+            }
+
+            .header-links {
+              flex-direction: column;
+              align-items: center;
+            }
+
+            .header-links a {
+              margin: 5px 0;
+            }
+          }
+        `}
+      </style>
     </>
   );
 });
