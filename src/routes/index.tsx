144584c96d9fea4513155413e81adbcf391c73a7
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-04-15 11:10:26 +0200

    updt index.tsx - changement du DocumentHead

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,11 +14,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Micky MARI",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Portfolio",
     },
   ],
 };
