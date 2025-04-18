8afb4766bdc227e0bbbdd3fe020d74c34382caaf
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-04-18 17:23:15 +0200

    updt about

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 ## Sources d'images
 [Github](https://github.com/logos)
 
-[Email](https://www.flaticon.com/fr/icones-gratuites/email)
-
 [LinkedIn](https://www.linkedin.com/in/your-profile/)
+
+[Email](https://www.flaticon.com/fr/icones-gratuites/email) Email icônes créées par Pixel perfect - Flaticon
+
+[Code](https://www.flaticon.com/fr/icones-gratuites/code) Code icônes créées par KP Arts - Flaticon
+
+[Musique](https://www.flaticon.com/fr/icones-gratuites/musique-google-play)
+ Musique google play icônes créées par Freepik - Flaticon
+
+[Jeux Vidéos](https://www.flaticon.com/fr/icones-gratuites/jeu-garcon-avance) Jeu garçon avance icônes créées par Freepik - Flaticon</a>
--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,42 +1,56 @@
 import Footer from "~/components/footer/footer";
 import Header from "~/components/header/header";
 import { component$ } from "@builder.io/qwik";
+import DevImage from "~/data/images/about/code.png?jsx";
+import MusicImage from "~/data/images/about/lecteur-de-musique.png?jsx";
+import GameImage from "~/data/images/about/jeux.png?jsx";
 import GithubMarkImage from "~/data/images/about/github-mark.png?jsx";
 import LinkedInLogoImage from "~/data/images/about/linkedin.png?jsx";
 import EmailImage from "~/data/images/about/email.png?jsx";
+import "./about.css"; // Importez le fichier CSS
 
 export default component$(() => {
   return (
     <>
       <Header />
-      <div style={{ fontSize: "1.5em", margin: "1em" }}>
+      <div class="about-container">
+        <strong>Qui suis-je</strong>
+        <p>
+          Salut, je suis Micky MARI actuellement en CMI OPTIM à l'université de
+          Bordeaux. J'aime apprendre de nouvelles choses et développer des
+          projets seul et avec mes amis.
+        </p>
         <strong>Passions</strong>
-        <ul></ul>
+        <ul>
+          <li>
+            <DevImage alt="Développement" />
+            Le développement
+          </li>
+          <li>
+            <MusicImage alt="Musique" />
+            La musique
+          </li>
+          <li>
+            <GameImage alt="Jeux vidéos" />
+            Les jeux vidéos
+          </li>
+        </ul>
         <strong>Contacts</strong>
         <ul>
-          <li style={{ display: "flex", alignItems: "center" }}>
-            <GithubMarkImage
-              alt="Github Mark"
-              style={{ width: "1em", height: "1em", borderRadius: "10%" }}
-            />
+          <li>
+            <GithubMarkImage alt="Github Mark" />
             <a href="https://github.com/pgii33" target="_blank">
               PGII33
             </a>
           </li>
-          <li style={{ display: "flex", alignItems: "center" }}>
-            <EmailImage
-              alt="Email"
-              style={{ width: "1em", height: "1em", borderRadius: "10%" }}
-            />
+          <li>
+            <EmailImage alt="Email" />
             <a href="mailto:micky.sg.mari@gmail.com" target="_blank">
               micky.sg.mari@gmail.com
             </a>
           </li>
-          <li style={{ display: "flex", alignItems: "center" }}>
-            <LinkedInLogoImage
-              alt="LinkedIn Logo"
-              style={{ width: "1em", height: "1em", borderRadius: "10%" }}
-            />
+          <li>
+            <LinkedInLogoImage alt="LinkedIn Logo" />
             <a href="https://www.linkedin.com/in/micky-mari/" target="_blank">
               https://www.linkedin.com/in/micky-mari/
             </a>
