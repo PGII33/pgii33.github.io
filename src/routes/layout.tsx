b2f4477a9a922e47ff63d65c27dc4d90d599958e
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-04-18 18:25:24 +0200

    footer en bas :) @untypequicode

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,22 +4,20 @@ import Header from "~/components/header/header";
 import Footer from "~/components/footer/footer";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.dev/docs/caching/
   cacheControl({
-    // Always serve a cached response by default, up to a week stale
     staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
     maxAge: 5,
   });
 };
 
 export default component$(() => {
   return (
-    <>
+    <div style="display: flex; flex-direction: column; min-height: 100vh;">
       <Header />
-      <Slot />
+      <main style="flex: 1">
+        <Slot />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 });
